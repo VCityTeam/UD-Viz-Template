ad1a718ceb75101f49dd809ffe8f3def11311d3e
Mathieu Livebardon <tsunoll96@gmail.com>
2023-04-20 11:59:11 +0200

    updade(webpackconfig): one file

--- packages/shared/webpack.config.js
+++ packages/shared/webpack.config.js
@@ -1,9 +1,63 @@
+const path = require('path');
+const mode = process.env.NODE_ENV;
+const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 
-const envConfig =
-  process.env.NODE_ENV === 'production'
-    ? require('./webpack.config.prod')
-    : require('./webpack.config.dev');
+require('dotenv').config({ path: '../../.env' });
 
-// We use this here at edX
-module.exports = merge(require('./webpack.config.common'), envConfig);
+const debugBuild = mode === 'development';
+
+const rules = [
+  {
+    // We also want to (web)pack the style files:
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader'],
+  },
+];
+
+// Inject environnement variables (they have to be declare in your .env !!!)
+const keyEnvVariables = ['MY_ENV_VARIABLE'];
+const plugins = [];
+const params = {};
+keyEnvVariables.forEach(function (key) {
+  console.log(key, ' = ', JSON.stringify(process.env[key]));
+  params[key] = JSON.stringify(process.env[key]);
+});
+plugins.push(new webpack.DefinePlugin(params));
+// possible de write in sources DEBUG flag that is going to be replace by a boolean during webpack
+plugins.push(
+  new webpack.DefinePlugin({
+    DEBUG: debugBuild,
+  })
+);
+
+const commonConfig = {
+  entry: path.resolve(__dirname, './src/bootstrap.js'),
+  output: {
+    filename: 'bundle.js',
+    library: 'myAppNameShared',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: rules,
+  },
+  plugins: plugins,
+};
+
+const devConfig = {
+  mode: 'development',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist/debug'),
+  },
+};
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist/release'),
+  },
+};
+
+module.exports = merge(commonConfig, debugBuild ? devConfig : prodConfig);
